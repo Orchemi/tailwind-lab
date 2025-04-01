75197a72cd29654901d8a983bd45712684688309
tmdgns1126 <tmdgns1126@naver.com>
2025-04-01 15:55:27 +0900

    Install: Tailwind Breakpoint Highlight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
 	return (
 		<div className="flex flex-col items-center justify-center h-screen">
 			<h1>Tailwind Lab</h1>
+			<button className="p-2 sm:p-3 md:p-4 lg:p-5 xl:p-6">Click me</button>
 		</div>
 	);
 }
